09dac13cdca9c0b0d600c05c73ca78cdc1b92826
Franklin Lopes <franklinlopes83@hotmail.com>
2022-07-18 21:00:17 -0300

    Iniciando projeto_evento back end

---
+++ public/exemplos/sintaxe_basica.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Básica PHP</title>
+</head>
+<body>
+    <h1>Exemplo de Texto</h1>
+    <?php
+    echo "<h1>Olá mundo</h1>";
+    $variavel = 10; // Criando uma Variavél
+    echo "\n<p>$variavel</p>";
+    
+     ?>
+</body>
+</html>
