24d28858d52a2c4f85fcc2da6ce2ba769fba8dd8
Franklin Lopes <franklinlopes83@hotmail.com>
2022-07-21 20:19:22 -0300

    finalizado funções de eventos

--- app/evento.php
+++ app/evento.php
@@ -1,17 +1,30 @@
 <?php
+date_default_timezone_set("America/Sao_Paulo");// Alterando o fuso horário do servidor
 
 $nomeEvento = $_POST["nomeEvento"];
 $dataEvento = $_POST["dataEvento"];
 
-echo " O {$nomeEvento} vai acontecer na data {$dataEvento}";
+echo "O {$nomeEvento} vai acontecer na data {$dataEvento}";
+
 echo "<hr>";
 
 // MOSTRANDO VETORES DE FORMA COMPLETA
-//print_r($_POST); 
+//print_r($_POST);
 var_dump($_POST);
 
-function validarData($data){
-$dataEvento = new DateTime($data); //Esra classe precisa de um padrão americano para funcioinar
-echo $dataEvento->format("d/m/Y"); //Mostrando a data no padrão brasileiro
+function validaData($data){
+
+    $dataEvento = new DateTime($data);// Esta classe precisa de uma data no padrão americano para funcionar
+    $dataAtual = new DateTime("now");// estamos pegando a data atual
+    echo $dataEvento->format("d/m/Y");// mostrando a data no padrão brasileiro
+    echo "<br> A data de hoje é: ";
+    print_r($dataAtual);
+
+    if($dataEvento > $dataAtual){
+        echo "<p>Data do evento cadastrado com sucesso!</p>";
+    }
+    else{
+        echo "<p>A data do evento não pode ser igual ou anterior à data atual</p>";
+    }
 }
-validarData($dataEvento);
+validaData($dataEvento);
